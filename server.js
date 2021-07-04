ce3a599385f5010658afc13f475ce4ad6ac2ab98
ManishS6 <63586933+ManishS6@users.noreply.github.com>
2021-07-04 11:42:07 +0530

    Updated for Heroku

--- server.js
+++ server.js
@@ -3,12 +3,28 @@ const app = express();
 const cors = require("cors");
 const admin = require("firebase-admin");
 const bodyParser = require("body-parser");
+const port = process.env.PORT || 8000;
+const { RSA_NO_PADDING } = require('constants');
+const saltedMd5=require('salted-md5');
+const path=require('path');
+const multer=require('multer')
+const upload=multer({storage: multer.memoryStorage()})
+require('dotenv').config()
+app.use(express.urlencoded())
+// app.use(compression())
+app.use(cors());
+app.use(express.json());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
 
 const serviceAccount = require("./confidential.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: process.env.BUCKET_URL
 });
+app.locals.bucket = admin.storage().bucket()
+
 const user = admin.auth().currentUser;
 if (user != null) {
   // The user object has basic properties such as display name, email, etc.
@@ -32,15 +48,6 @@ const db = admin.firestore()
 // One-Time update
 
 var docRef = db.collection("Rankawat").doc("Data");
-docRef.get().then((doc) => {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        console.log("No such document!");
-    }
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
 
 // Realtime-Updates from the firestore
 
@@ -48,3 +55,47 @@ docRef.get().then((doc) => {
 //     .onSnapshot((doc) => {
 //         console.log("Current data: ", doc.data());
 //     });
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+});
+app.get('/all',async (req,res)=>{
+// fetches the data once from firestore 
+  await db.collection("Rankawat").doc("Stock").get().then((doc) => {
+      if (doc.exists) {
+        res.json({
+          FireStore: doc.data()
+        })
+          // console.log("Document data:", doc.data());
+      } else {
+          console.log("No such document!");
+      }
+  }).catch((error) => {
+      console.log("Error getting document:", error);
+  });
+});
+
+app.post('/upload',upload.single('upfile'),async(req,res)=>{
+  const name = saltedMd5(req.file.originalname, 'SUPER-S@LT!')
+  const fileName = name + path.extname(req.file.originalname)
+  await app.locals.bucket.file(fileName).createWriteStream().end(req.file.buffer)
+  const price = parseInt(req.body.price);
+  let docRef=db.collection("Rankawat").doc("Stock").collection(req.body.Category).doc(req.body.tag)
+  await docRef.set({
+    Desc: req.body.desc,
+    Gold:req.body.Gtype,
+    Diamond:req.body.dtype,
+    Tag: req.body.tag,
+    Category: req.body.Category,
+    Price: price,
+    Img: fileName
+  });
+
+  res.json({
+    FBStorageURL: fileName,
+    status: "done"
+  });
+})
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
